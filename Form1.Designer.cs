acf1f182c5c639a42d254940f982501800c2d21d
yoga 11e <yoga 11e@DESKTOP-OOOUNS6>
2024-03-04 19:39:55 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Informacion_de_asignatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.txt_profesor = new System.Windows.Forms.TextBox();
+            this.txt_asignatura = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_informacion = new System.Windows.Forms.TextBox();
+            this.rb_presencial = new System.Windows.Forms.RadioButton();
+            this.rb_virtual = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.BackColor = System.Drawing.Color.MediumOrchid;
+            this.btn_aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_aceptar.Location = new System.Drawing.Point(580, 148);
+            this.btn_aceptar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(84, 44);
+            this.btn_aceptar.TabIndex = 0;
+            this.btn_aceptar.Text = "Aceptar";
+            this.btn_aceptar.UseVisualStyleBackColor = false;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // txt_profesor
+            // 
+            this.txt_profesor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_profesor.Location = new System.Drawing.Point(177, 115);
+            this.txt_profesor.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_profesor.Name = "txt_profesor";
+            this.txt_profesor.Size = new System.Drawing.Size(137, 26);
+            this.txt_profesor.TabIndex = 2;
+            this.txt_profesor.TextChanged += new System.EventHandler(this.txt_profesor_TextChanged);
+            // 
+            // txt_asignatura
+            // 
+            this.txt_asignatura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_asignatura.Location = new System.Drawing.Point(177, 169);
+            this.txt_asignatura.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_asignatura.Name = "txt_asignatura";
+            this.txt_asignatura.Size = new System.Drawing.Size(137, 26);
+            this.txt_asignatura.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(77, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Asignatura";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.BackColor = System.Drawing.Color.MediumOrchid;
+            this.btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(580, 95);
+            this.btn_salir.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(84, 40);
+            this.btn_salir.TabIndex = 6;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = false;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.MediumOrchid;
+            this.btn_limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpiar.Location = new System.Drawing.Point(580, 207);
+            this.btn_limpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(84, 37);
+            this.btn_limpiar.TabIndex = 7;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(322, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // txt_informacion
+            // 
+            this.txt_informacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_informacion.Location = new System.Drawing.Point(34, 295);
+            this.txt_informacion.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_informacion.Multiline = true;
+            this.txt_informacion.Name = "txt_informacion";
+            this.txt_informacion.Size = new System.Drawing.Size(614, 58);
+            this.txt_informacion.TabIndex = 9;
+            // 
+            // rb_presencial
+            // 
+            this.rb_presencial.AutoSize = true;
+            this.rb_presencial.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_presencial.Location = new System.Drawing.Point(384, 141);
+            this.rb_presencial.Margin = new System.Windows.Forms.Padding(2);
+            this.rb_presencial.Name = "rb_presencial";
+            this.rb_presencial.Size = new System.Drawing.Size(116, 28);
+            this.rb_presencial.TabIndex = 10;
+            this.rb_presencial.TabStop = true;
+            this.rb_presencial.Text = "Presencial";
+            this.rb_presencial.UseVisualStyleBackColor = true;
+            // 
+            // rb_virtual
+            // 
+            this.rb_virtual.AutoSize = true;
+            this.rb_virtual.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_virtual.Location = new System.Drawing.Point(384, 180);
+            this.rb_virtual.Margin = new System.Windows.Forms.Padding(2);
+            this.rb_virtual.Name = "rb_virtual";
+            this.rb_virtual.Size = new System.Drawing.Size(80, 28);
+            this.rb_virtual.TabIndex = 11;
+            this.rb_virtual.TabStop = true;
+            this.rb_virtual.Text = "Virtual";
+            this.rb_virtual.UseVisualStyleBackColor = true;
+            this.rb_virtual.CheckedChanged += new System.EventHandler(this.rb_virtual_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Profesor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MediumOrchid;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(196, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(228, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Cursos  informaticos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(379, 98);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Modalidad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(214, 244);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 25);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Informacion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(698, 442);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rb_virtual);
+            this.Controls.Add(this.rb_presencial);
+            this.Controls.Add(this.txt_informacion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_asignatura);
+            this.Controls.Add(this.txt_profesor);
+            this.Controls.Add(this.btn_aceptar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_aceptar;
+        private System.Windows.Forms.TextBox txt_profesor;
+        private System.Windows.Forms.TextBox txt_asignatura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_informacion;
+        private System.Windows.Forms.RadioButton rb_presencial;
+        private System.Windows.Forms.RadioButton rb_virtual;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Informacion_de_asignatura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_profesor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            string profesor, asignatura ,modalidad = " ";
+
+            asignatura = txt_asignatura.Text;
+            profesor = txt_profesor.Text;
+
+            if (rb_presencial.Checked)
+            {
+                modalidad = "Presencial";
+            }
+            else if (rb_virtual.Checked)
+            {
+                modalidad= "Virtual";
+            }
+
+            txt_informacion.Text = profesor + (" imparte la asignatura ") + asignatura + " en la modalidad " + modalidad;
+
+
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_asignatura.Clear();
+            txt_profesor.Clear();
+            txt_informacion.Clear();
+          
+       }
+
+        private void rb_virtual_CheckedChanged(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
